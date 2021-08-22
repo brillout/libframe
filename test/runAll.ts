c247f9410df6d8500b0461435b40516412807e06
Romuald Brillout <git@brillout.com>
2021-08-22 19:02:05 +0200

    wip

--- test/runAll.ts
+++ test/runAll.ts
@@ -1,10 +1,20 @@
 const execSync = require('child_process').execSync
 
-const args = process.argv.slice(2).join(' ')
+let args = process.argv.slice(2)
+let skipTs = false
+args = args.filter((arg) => {
+  if (arg === '--skipTs') {
+    skipTs = true
+    return false
+  }
+  return true
+})
 
-runScript('test:ts')
+if (!skipTs) {
+  runScript('test:ts')
+}
 runScript('test:jest')
 
 function runScript(scriptName: string) {
-  execSync(`yarn ${scriptName} ${args}`, { stdio: 'inherit' })
+  execSync(`yarn ${scriptName} ${args.join(' ')}`, { stdio: 'inherit' })
 }
