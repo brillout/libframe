840a3d9bda9cef52c0d8d2876dd830776228bcc8
Romuald Brillout <git@brillout.com>
2021-08-31 18:17:15 +0200

    also bump src

--- scripts/helpers/hasTest.ts
+++ scripts/helpers/hasTest.ts
@@ -1,9 +1,16 @@
 import { readdirSync } from 'fs'
 import assert = require('assert')
+import { isAbsolute } from 'path'
+import { DIR_SRC } from '../helpers/locations'
 
 export { hasTest }
 
 function hasTest(dir: string): boolean {
+  assert(isAbsolute(dir))
+  assert(isAbsolute(DIR_SRC))
+  if (dir === DIR_SRC) {
+    return true
+  }
   const files = readdirSync(dir)
   assert(files.some((file) => file === 'package.json'))
   return files.some((file) => file.includes('.test'))
