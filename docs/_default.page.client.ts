3561491fa9c3a5ee2a90401141a2e45fd611389b
Jaden Gregory <dev@jadengregory.com.au>
2021-12-13 21:36:57 +1100

    Import client side Algolia js and css

--- docs/_default.page.client.ts
+++ docs/_default.page.client.ts
@@ -3,7 +3,8 @@ import './css/index.css'
 import './autoScrollNav'
 import './installSectionUrlHashs'
 import './Navigation.client'
-
+import './AlgoliaSearch.client'
+import './AlgoliaSearch.css'
 /*
 export { setClientFrame }
 function setClientFrame({assets}: {assets: string[]}) {
