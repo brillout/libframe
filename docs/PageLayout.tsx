a285ce8d8bbc76849d4a35243a0dc4f83383cbf1
Jaden Gregory <dev@jadengregory.com.au>
2021-12-13 21:37:36 +1100

    Add component to page layout

--- docs/PageLayout.tsx
+++ docs/PageLayout.tsx
@@ -3,6 +3,7 @@ import { Navigation } from './Navigation'
 import type { Heading } from './processPageContext'
 import { MobileHeader } from './MobileHeader'
 import { EditPageNote } from './EditPageNote'
+import { AlgoliaSearch } from './AlgoliaSearch'
 /* Won't work this this file is loaded only on the server
 import './PageLayout.css'
 */
@@ -23,20 +24,23 @@ function PageLayout({
 }) {
   const { headingsWithSubHeadings, isLandingPage, pageTitle } = pageContext
   return (
-    <div
-      style={{
-        display: 'flex',
-      }}
-    >
-      <Navigation headingsWithSubHeadings={headingsWithSubHeadings} urlPathname={pageContext.urlPathname} />
-      <div id="page-container" className={isLandingPage ? '' : 'doc-page'}>
-        <MobileHeader />
-        <div id="page-content">
-          {pageTitle && <h1>{pageTitle}</h1>}
-          {children}
-          {!isLandingPage && <EditPageNote pageContext={pageContext} />}
+    <>
+      <AlgoliaSearch />
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
+        <Navigation headingsWithSubHeadings={headingsWithSubHeadings} urlPathname={pageContext.urlPathname} />
+        <div id="page-container" className={isLandingPage ? '' : 'doc-page'}>
+          <MobileHeader />
+          <div id="page-content">
+            {pageTitle && <h1>{pageTitle}</h1>}
+            {children}
+            {!isLandingPage && <EditPageNote pageContext={pageContext} />}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
