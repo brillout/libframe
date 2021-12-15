ddefbb4ac049303af0bea565d3397900c1022f77
Romuald Brillout <git@brillout.com>
2021-12-15 14:41:33 +0100

    feat

--- docs/components/ReadingRecommendation.tsx
+++ docs/components/ReadingRecommendation.tsx
@@ -4,14 +4,23 @@ import { Link } from './Link'
 
 export { ReadingRecommendation }
 
-function ReadingRecommendation({ links }: { links: string[] }) {
+function ReadingRecommendation({ tour, links }: { tour?: true; links: string[] }) {
   return (
     <Info>
-      <b>Reading Recommendations.</b>
-      We recommend to be familiar with following documents before proceeding.
-      {links.map((link, i) => (
-        <Link key={i} href={link} />
-      ))}
+      <b>Reading Recommendation.</b>
+      <p>We recommend to be familiar with the following before proceeding.</p>
+      <ul>
+        {tour && (
+          <li>
+            <Link href={'/react-tour'} noBreadcrumb={true} /> or <Link href={'/vue-tour'} noBreadcrumb={true} />
+          </li>
+        )}
+        {links.map((link, i) => (
+          <li>
+            <Link key={i} href={link} />
+          </li>
+        ))}
+      </ul>
     </Info>
   )
 }
