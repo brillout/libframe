21cb1f6babea9bf13b8fb9e66f5ab29feb899c2b
Romuald Brillout <git@brillout.com>
2021-12-21 22:25:43 +0100

    minor

--- docs/components/Note.tsx
+++ docs/components/Note.tsx
@@ -4,8 +4,10 @@ export { Note }
 
 function Note(props: { icon: JSX.Element; children: JSX.Element }) {
   return (
-    <blockquote style={{ paddingTop: 20, paddingBottom: 20 }}>
+    <blockquote>
+      <div style={{ marginBottom: 20 }} />
       {props.icon} {props.children}
+      <div style={{ marginTop: 20 }} />
     </blockquote>
   )
 }
