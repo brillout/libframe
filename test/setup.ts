9ecd3ccfc88347ab0dcf819e97694e3e211f68c4
Romuald Brillout <git@brillout.com>
2022-01-10 13:11:49 +0100

    increase test timeout

--- test/setup.ts
+++ test/setup.ts
@@ -22,7 +22,7 @@ export { sleep }
 
 const TIMEOUT_NPM_SCRIPT = 30 * 1000 * (!isGithubAction() ? 1 : isLinux() ? 1 : 4)
 const TIMEOUT_JEST = 30 * 1000 * (!isGithubAction() ? 1 : isLinux() ? 5 : 6)
-const TIMEOUT_AUTORETRY = 10 * 1000 * (!isGithubAction() ? 1 : isLinux() ? 1 : 6)
+const TIMEOUT_AUTORETRY = 10 * 1000 * (!isGithubAction() ? 1 : isLinux() ? 1 : 10)
 const TIMEOUT_PROCESS_TERMINATION = 10 * 1000 * (!isGithubAction() ? 1 : isLinux() ? 1 : 2)
 const TIMEOUT_PLAYWRIGHT = TIMEOUT_JEST
 //const TIMEOUT_PAGE_LOAD = TIMEOUT_PLAYWRIGHT
