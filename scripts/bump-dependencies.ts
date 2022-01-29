94908a3d2b358734638f966620dc2fbb08e28317
Romuald Brillout <git@brillout.com>
2022-01-29 21:31:04 +0100

    prettier

--- scripts/bump-dependencies.ts
+++ scripts/bump-dependencies.ts
@@ -12,7 +12,15 @@ const FREEZE_VUE = true
 const FREEZE_VUE = false
 //*/
 
-const SKIP_LIST = ['node-fetch', '@types/node-fetch', 'p-limit', 'react-router-dom', 'react-router', 'webpack', 'miniflare']
+const SKIP_LIST = [
+  'node-fetch',
+  '@types/node-fetch',
+  'p-limit',
+  'react-router-dom',
+  'react-router',
+  'webpack',
+  'miniflare',
+]
 
 if (FREEZE_VUE) {
   SKIP_LIST.push(...['vue', '@vue/server-renderer', '@vue/compiler-sfc', '@vitejs/plugin-vue', 'vite-plugin-md'])
