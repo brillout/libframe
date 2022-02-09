833c5e4df2ba4db3d7b49b7787d1183154b8f899
Romuald Brillout <git@brillout.com>
2022-02-09 11:08:29 +0100

    update release script

--- scripts/helpers/locations.ts
+++ scripts/helpers/locations.ts
@@ -1,25 +1,18 @@
+import * as fs from 'fs'
 import * as assert from 'assert'
-
-export { getNpmName }
 
 export const DIR_ROOT = `${__dirname}/../../..`
 export const DIR_BOILERPLATES = getNpmName() === 'telefunc' ? null : `${DIR_ROOT}/boilerplates`
 export const DIR_EXAMPLES = `${DIR_ROOT}/examples`
 export const DIR_SRC = getSrcDir()
-
-type PackageJson = { workspaces: string[] }
-function getRootPackageJson() {
-  const pkg = require(`${DIR_ROOT}/package.json`) as PackageJson
-  return pkg
-}
+export { getNpmName }
 
 function getSrc() {
-  const pkg = getRootPackageJson()
-  const { workspaces } = pkg
-  const srcS = workspaces.filter((workspace) => workspace === 'vite-plugin-ssr/' || workspace === 'telefunc/')
-  assert(srcS.length === 1, String(workspaces))
+  const packages = getMonorepoPackages()
+  const srcS = packages.filter((name) => name === 'vite-plugin-ssr/' || name === 'telefunc/')
+  assert(srcS.length === 1, String(packages))
   const src = srcS[0].slice(0, -1)
-  assert(src === 'vite-plugin-ssr' || src === 'telefunc', String(workspaces))
+  assert(src === 'vite-plugin-ssr' || src === 'telefunc', String(packages))
   return src
 }
 
@@ -29,4 +22,25 @@ function getSrcDir() {
 
 function getNpmName(): 'vite-plugin-ssr' | 'telefunc' {
   return getSrc()
+}
+
+function getMonorepoPackages(): string[] {
+  const workspaceYaml = fs.readFileSync(`${DIR_ROOT}/pnpm-workspace.yaml`, 'utf8')
+  const packages = []
+  workspaceYaml
+    .split('\n')
+    .filter(Boolean)
+    .forEach((line) => {
+      if (line === 'packages:') {
+        return
+      } else if (line.startsWith("  - '")) {
+        assert(line.endsWith("'"))
+        const parts = line.split("'")
+        assert(parts.length === 3 && parts[2] === '', JSON.stringify({ line, parts }))
+        packages.push(parts[1])
+      } else {
+        assert(false, 'line: ' + line)
+      }
+    })
+  return packages
 }
